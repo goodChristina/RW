047665343538d5edae1d736447b05cdd85fbc96d
goodChristina <eponymouswoman@gmail.com>
2025-11-20 01:17:36 +0000

    extract substring added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 
 void main() {
-  IO.println(String.format("Hello!"));
+  //IO.println(String.format("Hello!"));
+    String originalText = "This is a string I will extract a substring from";
+    String sub = originalText.substring(10,17);
+    System.out.println(sub);
+
 }
