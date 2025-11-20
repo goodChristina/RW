23335754e0639c7c4b81f532e2265c5bc4701800
goodChristina <eponymouswoman@gmail.com>
2025-11-20 01:56:05 +0000

    too many mains

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,34 @@
-
 import java.util.Scanner;
 
 void main() {
-  //IO.println(String.format("Hello!"));
-    String originalText = "This is a string I will extract a substring from";
-    String sub = originalText.substring(10,17);
-    System.out.println(sub);
+        String originalText = "This is a string I will extract a substring from!";
+        String sub = originalText.substring(10, 17);
+        System.out.println(sub);
 
-}
-
-// Validate User Commands
-
-    public static boolean checkifEmail(String email) {
-        if (email == null || email.isEmpty()) {
-            return false;
-        }
-
-        //String reEmail = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        String reEmail = "^(?i)[a-z0-9.!#$%&'*+/=?^_`{|}~-]+" +
-                "@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?" +
-                "(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$";
-        return email.matches(reEmail);
-
-    }
-     void main(String[] args) {
+        // check email
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter your email address: ");
         String userEmail = scanner.nextLine();
 
-        if (checkifEmail(userEmail)) {
+        if (checkIfEmail(userEmail)) {
             System.out.println("Email is valid!");
         } else {
             System.out.println("Invalid email. Please try again.");
         }
 
         scanner.close();
+    }
+
+
+    // using regex
+    public static boolean checkIfEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        String reEmail =
+                "^(?i)[a-z0-9.!#$%&'*+/=?^_`{|}~-]+" + "@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?" + "(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$";
+        return email.matches(reEmail);
     }
 
